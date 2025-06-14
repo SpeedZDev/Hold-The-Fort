15724220a397c28aed947e4e453f18ee9d6980f8
SpeedZDev <boblovesdogs321@gmail.com>
2025-06-14 12:57:13 -0400

    added more stuff

--- Assets/Scripts/MoneyText.cs
+++ Assets/Scripts/MoneyText.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class MoneyText : MonoBehaviour
 {
-    // Start is called before the first frame update
+    TMP_Text Moneytext;
     void Start()
     {
-        
+        Moneytext = GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Moneytext.text = "Money: " + MoneyManager.instance.Money;
     }
 }
